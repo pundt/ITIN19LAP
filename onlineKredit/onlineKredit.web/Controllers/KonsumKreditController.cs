cefdff36d63c241d1a806b9251fb6a5a892a0873
Pilgerstorfer Franz <franz.pilgerstorfer@pundt.at>
2016-11-08 10:03:08 +0100

    initial commit

---
+++ onlineKredit/onlineKredit.web/Controllers/KonsumKreditController.cs
@@ -0,0 +1,84 @@
+﻿using onlineKredit.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace onlineKredit.web.Controllers
+{
+    public class KonsumKreditController : Controller
+    {
+        [HttpGet]
+        public ActionResult KreditRahmen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KreditRahmen(KreditRahmenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FinanzielleSituation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FinanzielleSituation(FinanzielleSituationModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PersönlicheDaten()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersönlicheDaten(PersönlicheDatenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Arbeitgeber()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Arbeitgeber(ArbeitgeberModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KontoInformationen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KontoInformationen(KontoInformationenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Zusammenfassung()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Zusammenfassung(ZusammenfassungModel model)
+        {
+            return View();
+        }
+    }
+}
